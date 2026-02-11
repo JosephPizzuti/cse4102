418ca016eed2f73cc1da0ffb134ff64298712f93
Joseph <jpsocial20@gmail.com>
2026-02-11 12:20:01 -0500

    added hw1 to repo / completed three sum Q's + fastFib

---
+++ hw1/warmup.ml
@@ -0,0 +1,45 @@
+(** UConn CSE 4102 **)
+(** Spring 2026 **)
+(** Homework 1 **)
+(** Warmup **)
+
+let rec sum (n: int) : int =
+  match n with
+  | 0 -> 0
+  | n -> n + sum(n-1)
+
+let rec sumsq (n: int) : int =
+  match n with
+  | 0 -> 0
+  | n -> (n * n) + sumsq(n-1)
+
+let rec sumOdd (n: int) : int =
+  match n with
+  | 1 -> 1
+  | n -> if (n % 2 = 0) then sumOdd(n-1) else n + sumOdd(n-1)
+
+let rec fib (n: int) : int =
+  if n <= 1 then 1
+  else fib (n - 1) + fib (n - 2)
+
+let fibFast n =
+  match n with
+  | _ when n < 1 -> 0
+  | 1 -> 1
+  | _ ->
+      let rec aux n a b =
+        if n = 0 then a
+        else aux (n-1) b (a+b)
+      in aux n 0 1
+
+let sinappx (n: int) (x : float) : float =
+  raise Util.Unimplemented
+
+let rec repeat (c: char) (n: int) : char list =
+  raise Util.Unimplemented
+
+let rec run_length_encode (l : char list) : (char * int) list =
+  raise Util.Unimplemented
+
+let rec run_length_decode (l: (char * int) list) : char list =
+    raise Util.Unimplemented
