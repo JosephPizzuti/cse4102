f7ee410b11b4e8a9a29ce52185b13db38ba1610b
Joseph <jpsocial20@gmail.com>
2026-02-12 00:09:05 -0500

    finished all problems / now adding assert test statements to verify work

--- hw1/warmup.ml
+++ hw1/warmup.ml
@@ -16,7 +16,7 @@ let rec sumsq (n: int) : int =
 let rec sumOdd (n: int) : int =
   match n with
   | 1 -> 1
-  | n -> if (n % 2 = 0) then sumOdd(n-1) else n + sumOdd(n-1)
+  | n -> if (n mod 2 = 0) then (sumOdd (n-1)) else n + (sumOdd (n-1))
 
 let rec fib (n: int) : int =
   if n <= 1 then 1
@@ -44,10 +44,37 @@ let sinappx (n: int) (x : float) : float =
   else aux 1 x x
 
 let rec repeat (c: char) (n: int) : char list =
-  raise Util.Unimplemented
+  match n with
+  | 0 -> []
+  | n -> c :: repeat c (n-1)
 
 let rec run_length_encode (l : char list) : (char * int) list =
-  raise Util.Unimplemented
+  let rec aux curr_letter count list acc =
+    match list with
+    | [] -> (curr_letter,count) :: acc
+    | h::t ->
+        if h = curr_letter then aux curr_letter (count+1) t acc
+        else aux h 1 t ((curr_letter,count) :: acc) 
+in match l with
+| [] -> []
+| h::t ->
+    List.rev (aux h 1 t [])
 
 let rec run_length_decode (l: (char * int) list) : char list =
-    raise Util.Unimplemented
+  let rec aux list acc =
+    match list with
+    | [] -> acc
+    | (char,count)::t ->
+        if count = 0 then aux t acc
+        else aux ((char,count-1)::t) (char::acc)
+in match l with
+| [] -> []
+| l ->
+    List.rev (aux l [])
+
+let () =
+  assert (sum 0 = 0);
+  assert (sum 10 = 55);
+  
+  assert (sumsq 0 = 0);
+  assert (sumsq 5 = 55);
